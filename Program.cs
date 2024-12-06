b5770a8106a215093c7bd3efd9e0e1c3acb04c54
Sebastian Weiske <account@sweiske.de>
2024-12-06 13:18:05 +0100

    feat(): Program.cs

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2024
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Day1 Part1");
+            Day1.Day1Part1.Start();
+            Console.WriteLine("Day1 Part2");
+            Day1.Day1Part2.Start();
+            Console.WriteLine("Day2 Part1");
+            Day2.Day2Part1.Start();
+            Console.WriteLine("Day3 Part1");
+            Day3.Day3Part1.Start();
+            Console.WriteLine("Day3 Part2");
+            Day3.Day3Part2.Start();
+        }
+    }
+}

d3bcd916d936426f6ab4f3ff7d35fea1809084d9
Sebastian Weiske <account@sweiske.de>
2024-12-09 13:40:34 +0100

    feat(): Day6 Part1

--- Day6/Day6Part1.cs
+++ Day6/Day6Part1.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Security;
 using System.Security.Cryptography;
 
@@ -8,13 +9,13 @@ namespace AdventOfCode2024.Day6
         public static void Start() 
         {
             string filePath = @"../../../Day6/input.txt";
-            var convert = ConvertToChar(filePath);
+            var convert = ConvertToArray(filePath);
             int[,] array = convert.Item1;
-            (int, int) character = convert.Item2;
-            Console.WriteLine()
-            Walking(array, character);
+            (int, int) startPoint = convert.Item2;
+            int answer = Walking(array, startPoint);
+            Console.WriteLine($"Answer {answer}");
         }
-        private static (int[,], (int, int)) ConvertToChar(string filePath)
+        private static (int[,], (int, int)) ConvertToArray(string filePath)
         {
             try
             {
@@ -41,27 +42,96 @@ namespace AdventOfCode2024.Day6
                         }
                     }
                 }
-                for (int i = 0; i < array.GetLength(0); i++)
-                {
-                   
-                    for (int j = 0; j < array.GetLength(1); j++)
-                    {
-                        Console.Write(" " + array[i, j]);
-                    }
-                    Console.WriteLine(",");
-           
-                }
                 return (array, character);
             }
             catch (Exception ex)
             {
                 Console.WriteLine($"Fehler beim Lesen der Datei: {ex.Message}");
-                return null;
+                return (null, (0,0));
             }
         }
-        private static void Walking(int[,] array, (int, int) character) 
+        private static int Walking(int[,] array, (int, int) startPoint) 
         {
-
+            int bufferLeft = startPoint.Item1;
+            int bufferRight = startPoint.Item2;
+            char[] directions = ['U', 'R', 'D', 'L'];
+            char direction = directions[0];
+            int index = 0;
+            array[bufferLeft, bufferRight] = 3; //StartPunkt vom Spieler setzten
+            while (true)
+            {
+                bool moved = false;
+                if (direction == 'U' && bufferLeft - 1 < array.GetLength(0) && bufferLeft != 0)
+                {
+                    if (array[bufferLeft -1, bufferRight] == 1)
+                    {
+                        index++;
+                        direction = directions[index];
+                    }
+                    else
+                    {
+                        bufferLeft--;
+                        array[bufferLeft, bufferRight] = 3;
+                    }
+                    moved = true;
+                }
+                if (direction == 'R' && bufferRight + 1 < array.GetLength(1) && bufferRight != 0)
+                {
+                    if (array[bufferLeft, bufferRight + 1] == 1)
+                    {
+                        index++;
+                        direction = directions[index];
+                    }
+                    else
+                    {
+                        bufferRight++;
+                        array[bufferLeft, bufferRight] = 3;
+                    }
+                    moved = true;
+                }
+                if (direction == 'D' && bufferLeft + 1 < array.GetLength(0) && bufferLeft != 0)
+                {
+                    if (array[bufferLeft + 1, bufferRight] == 1)
+                    {
+                        index++;
+                        direction = directions[index];
+                    }
+                    else
+                    {
+                        bufferLeft++;
+                        array[bufferLeft, bufferRight] = 3;
+                    }
+                    moved = true;
+                }
+                if (direction == 'L' && bufferRight - 1 < array.GetLength(1) && bufferRight != 0)
+                {
+                    if (array[bufferLeft, bufferRight -1] == 1)
+                    {
+                        index = 0;
+                        direction = directions[index];
+                    }
+                    else
+                    {
+                        bufferRight--;
+                        array[bufferLeft, bufferRight] = 3;
+                    }
+                    moved = true;
+                }
+                if (!moved)
+                {
+                    int count = 0;
+                    for (int i = 0; i < array.GetLength(0); i++)
+                    {
+                        for (int j = 0; j < array.GetLength(1); j++)
+                        {
+                           if ((array[i, j] == 3)) {
+                                count++;
+                           }
+                        }
+                    }
+                    return count;
+                }
+            }
         }
     }
 }

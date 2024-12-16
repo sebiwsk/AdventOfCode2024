f122ab8bcbed9c9f57ffe70bcea6492fda46ccab
Sebastian Weiske <account@sweiske.de>
2024-12-16 14:18:48 +0100

    feat(): Day9 Part1

---
+++ Day9/Day9Part1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace AdventOfCode2024.Day9
+{
+    public class Day9Part1
+    {
+        public static void Start()
+        {
+            string filePath = @"..\..\..\Day9\input.txt";
+            int[] convert = ConvertToString(filePath);
+            var convertToList = ConvertToList(convert);
+            List<int> list = convertToList.Item1;
+            int count = convertToList.Item2;
+            MoveElement(list, count);
+            long answer = Checksum(list);
+            Console.WriteLine("Answer: " + answer);
+        }
+        private static int[] ConvertToString(string filePath)
+        {
+            try
+            {
+                string content = File.ReadAllText(filePath);
+                int[] ints = new int[content.Length];
+                for (int i = 0; i < content.Length; i++)
+                {
+                    ints[i] = content[i] - '0';
+                }
+                return ints;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Fehler beim Lesen der Datei: {ex.Message}");
+                return null;
+            }
+        }
+        private static (List<int>, int) ConvertToList(int[] input)
+        {
+            List<int> intList = new List<int>();
+            int id = 0;
+            int count = 0;
+            for(int i = 0; i < input.Length;  i++)
+            {
+                if (i % 2 == 0)
+                {
+                    for (int j = 0; j < input[i]; j++)
+                    {
+                        intList.Add(id);
+                    }
+                    id++;
+                }
+                else
+                {
+                    for(int j = 0; j < input[i]; j++)
+                    {
+                        intList.Add(-1);
+                        count++;
+                    }
+                }
+            }
+            return (intList, count);
+        }
+        private static void MoveElement(List<int> intList, int border)
+        {
+            bool finished = false;
+            int count = 0;
+            while (finished == false)
+            {
+                for (int i = intList.Count - 1; i > 0;i--)
+                {
+                    if (intList[i] != -1 && i > 0)
+                    {
+                        int buffer = intList[i];
+                        int bufferj = 0;
+                        for (int j = 0; j < intList.Count; j++)
+                        {
+                            if (count == border)
+                            {
+                                finished = true;
+                                break;
+                            }
+                            if (intList[j] == -1)
+                            {
+                                count++;
+                                bufferj = intList[j];
+                                intList[j] = buffer;
+                                break;
+                            }
+                        }
+                        if (finished == false) {
+                            intList[i] = bufferj;
+                        }
+                        break;
+                    }
+                }
+            }
+        }
+        private static long Checksum(List<int> list)
+        {
+            long answer = 0;
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (list[i] != -1 )
+                {
+                    answer += i * list[i];
+                }
+            }
+            return answer;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,30 +10,32 @@ namespace AdventOfCode2024
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Day1 Part1");
-            Day1.Day1Part1.Start();
-            Console.WriteLine("Day1 Part2");
-            Day1.Day1Part2.Start();
-            Console.WriteLine("Day2 Part1");
-            Day2.Day2Part1.Start();
-            Console.WriteLine("Day2 Part2");
-            Day2.Day2Part2.Start();
-            Console.WriteLine("Day3 Part1");
-            Day3.Day3Part1.Start();
-            Console.WriteLine("Day3 Part2");
-            Day3.Day3Part2.Start();
-            Console.WriteLine("Day4 Part1");
-            Day4.Day4Part1.Start();
-            Console.WriteLine("Day4 Part2");
-            Day4.Day4Part2.Start();
-            Console.WriteLine("Day5 Part1");
-            Day5.Day5Part1.Start();
-            Console.WriteLine("Day6 Part1");
-            Day6.Day6Part1.Start();
-            Console.WriteLine("Day7 Part1");
-            Day7.Day7Part1.Start();
-            Console.WriteLine("Day7 Part2");
-            Day7.Day7Part2.Start();
+            //Console.WriteLine("Day1 Part1");
+            //Day1.Day1Part1.Start();
+            //Console.WriteLine("Day1 Part2");
+            //Day1.Day1Part2.Start();
+            //Console.WriteLine("Day2 Part1");
+            //Day2.Day2Part1.Start();
+            //Console.WriteLine("Day2 Part2");
+            //Day2.Day2Part2.Start();
+            //Console.WriteLine("Day3 Part1");
+            //Day3.Day3Part1.Start();
+            //Console.WriteLine("Day3 Part2");
+            //Day3.Day3Part2.Start();
+            //Console.WriteLine("Day4 Part1");
+            //Day4.Day4Part1.Start();
+            //Console.WriteLine("Day4 Part2");
+            //Day4.Day4Part2.Start();
+            //Console.WriteLine("Day5 Part1");
+            //Day5.Day5Part1.Start();
+            //Console.WriteLine("Day6 Part1");
+            //Day6.Day6Part1.Start();
+            //Console.WriteLine("Day7 Part1");
+            //Day7.Day7Part1.Start();
+            //Console.WriteLine("Day7 Part2");
+            //Day7.Day7Part2.Start();
+            Console.WriteLine("Day9 Part1");
+            Day9.Day9Part1.Start();
         }
     }
 }
